6f759f5d065fddb2682f6b24390bf0665ed074bc
Remy Bernard Linux Mint <remy.bernard.canada@gmail.com>
2024-11-17 16:54:14 -0500

    added vuejs integration to astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "en",
-        locales: ["fr", "en"],
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: true
-        }        
-      }
+  i18n: {
+      defaultLocale: "en",
+      locales: ["fr", "en"],
+      routing: {
+          prefixDefaultLocale: true,
+          redirectToDefaultLocale: true
+      }        
+    },
+
+  integrations: [vue()]
 });
