f342f50ef63a730fe39f99384ac98c1e47acad87
Remy Bernard Linux Mint <remy.bernard.canada@gmail.com>
2024-11-17 15:02:55 -0500

    site structure, i18n, and english content

---
+++ src/i18n/utils.js
@@ -0,0 +1,22 @@
+// detects the page languaged based on URL
+// from https://docs.astro.build/en/recipes/i18n/
+export function getLangFromUrl(url) {
+    const defaultLang = "en";
+    const [, lang] = url.pathname.split('/');
+    if (lang === "en" || lang === "fr") return lang;
+    return defaultLang;
+}
+
+// Translates a URL to the other language (theLang)
+// E.g. if the URL is the english path, it will return the same path but for the french language
+export function translateURL(url, theLang) {
+    const urlParts = url.pathname.split('/');
+
+    // Replace the second element with the new language code
+    if (urlParts.length > 1) {
+        urlParts[1] = theLang;
+    }
+
+    // Join the array back into a string using '/' as the delimiter
+    return urlParts.join('/');
+  }
